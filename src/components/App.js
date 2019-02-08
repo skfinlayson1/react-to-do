7320ce56e9e961086b80961a3809d23244b696cc
Shane Finlayson <shane_finlayson@yahoo.com>
2019-02-07 18:32:09 -0700

    Checkpoint 6 Components: Completed

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ToDoList from './ToDo'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ToDoList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ToDo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ToDoList extends React.Component {
+    
+    constructor() {
+        super()
+        this.state = '';
+    }
+
+    render() {
+        return (
+            <h1>Hello!</h1>
+        )
+    }
+}
+
+export default ToDoList;
